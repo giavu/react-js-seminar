a8b658607ab635a4d6307a77256cface0b67ebfa
giavu <giaphongvu@gmail.com>
2018-11-20 12:33:23 -0800

    Updates

---
+++ precache-manifest.0fba3dfe8f987c52312c635b4a21ff63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bf5aee7508726ca4c39",
+    "url": "/react-js-seminar/static/js/runtime~main.6bf5aee7.js"
+  },
+  {
+    "revision": "70960a9c448c47b735cf",
+    "url": "/react-js-seminar/static/js/main.70960a9c.chunk.js"
+  },
+  {
+    "revision": "22f90f3f0b71aaf0653e",
+    "url": "/react-js-seminar/static/js/1.22f90f3f.chunk.js"
+  },
+  {
+    "revision": "70960a9c448c47b735cf",
+    "url": "/react-js-seminar/static/css/main.a0247e10.chunk.css"
+  },
+  {
+    "revision": "0e26ac0a9b06fb26357258017a16d409",
+    "url": "/react-js-seminar/index.html"
+  }
+];
